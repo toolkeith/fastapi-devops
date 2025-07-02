a1e15969525075572c33ab7f63b8ecad35a89c36
toolkeith <sevillakeithbryanv@gmail.com>
2025-07-03 05:49:56 +0800

    Added changes in main.py for the CICD to fail

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from typing import List
 
-app = FastAPI()
+app = FastAPI();
 
 origins = [
     "http://localhost.tiangolo.com",
